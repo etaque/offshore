0094ef954ab0700940c158aedae02d47de015ed3
Erwan Loisant <eloisant@gmail.com>
2016-11-22 15:41:40 +0100

    Full screen, draggable map

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,42 @@
+import R from 'ramda';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import h from 'react-hyperscript';
 import MapGL from 'react-map-gl';
 
 class App extends React.Component {
-  render() {
-    return h(MapGL, {
-      width: 800,
-      height: 600,
-      latitude: 37.7577,
-      longitude: -122.4376,
-      zoom: 8,
-      onChangeViewport: (viewport) => {
-        const {latitude, longitude, zoom} = viewport;
-        // Optionally call `setState` and use the state to update the map.
+
+  state = {}
+
+  _onChangeViewport = (opt) => {
+    if (this.props && this.props.onChangeViewport) {
+      return this.props.onChangeViewport(opt);
+    }
+    this.setState({
+      viewport: {
+        latitude: opt.latitude,
+        longitude: opt.longitude,
+        zoom: opt.zoom,
+        startDragLngLat: opt.startDragLngLat,
+        isDragging: opt.isDragging
       }
     });
   }
+
+  render() {
+    return h(MapGL, R.merge({
+      mapboxApiAccessToken: 'pk.eyJ1IjoiZWxvaXNhbnQiLCJhIjoiY2l2dGt6MDJoMDAyYzJ6bDRmMXdtNWE2ciJ9.ph6CziqSFFnHUzc35qvuzw',
+      width: window.innerWidth,
+      height: window.innerHeight,
+      latitude: 38,
+      longitude: -46,
+      startDragLngLat: null,
+      zoom: 3,
+      onChangeViewport: this._onChangeViewport
+    }, this.state.viewport));
+  }
+
 }
 
 ReactDOM.render(React.createElement(App, {}), document.getElementById('container'));
