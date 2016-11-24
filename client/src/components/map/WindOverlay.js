4e7e2e0d853270b503a5522496b07a9a61dd78b0
Erwan Loisant <eloisant@gmail.com>
2016-11-24 15:36:55 +0100

    Respect speed and direction for wind segments

--- client/src/components/map/WindOverlay.js
+++ client/src/components/map/WindOverlay.js
@@ -37,13 +37,16 @@ class WindOverlay extends React.Component {
   getData() {
     return R.map((windCell) => {
       // offset is used to prevent all segments to start and end at the same place
-      const offset = Math.abs(this.state.time + Math.round(windCell[1])*10 + Math.round(windCell[0])*10) % 50;
-      const long = windCell[1] + 0.05 * offset;
-      const lat = windCell[0] + 0.05 * offset;
-      const opacity = 255 - (Math.abs(offset - 25) * 255 / 25);
+      // We generate a [-25, 25] value so the segment occillate around its position
+      const offset = Math.abs(this.state.time + Math.round(windCell[1])*10 + Math.round(windCell[0])*10) % 50 - 25;
+      const u = windCell[2];
+      const v = windCell[3] / 10; // normalized, otherwise segments are too big
+      const long = windCell[1] + (0.05 * offset * Math.cos(u));
+      const lat = windCell[0] + (0.05 * offset * Math.sin(u));
+      const opacity = 255 - (Math.abs(offset) * 255 / 25);
       return {
         sourcePosition: [long, lat],
-        targetPosition: [long + 1, lat + 1],
+        targetPosition: [long + (v * Math.cos(u)), lat + (v * Math.sin(u))],
         color: [0, 0, 150, opacity]
       };
     }, this.props.wind);
